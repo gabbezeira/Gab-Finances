580d96b7c0c828e1f5735cb59fd5cd867a6e94ad
Gabriel  Alves <68339733+GabrielWeises@users.noreply.github.com>
2021-12-16 09:48:09 -0300

    Update Public.tsx

--- src/routes/Public.tsx
+++ src/routes/Public.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
 
-export function Public() {
+import { Header } from "../components/Header";
+import { NewTransactionModal } from "../components/NewTransactionModal";
+
+export function Private() {
+  const [
+    isNewTransactionModalOpen,
+    setIsNewTransactionModalOpen
+  ] = useState<boolean>(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <BrowserRouter>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Redirect from="*" to="/" />
